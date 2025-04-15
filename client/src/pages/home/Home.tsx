6b37288a5be5973950ca31630936cf31f7ee315b
Przemysław Zieliński <zprzemek378@gmail.com>
2025-04-15 15:50:34 +0200

    shadcn configuration

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 const Home = () => {
-  return <div className="bg-red-500">Home</div>;
+  return (
+    <div>
+      <Button variant="secondary">siema</Button>
+    </div>
+  );
 };
 
 export default Home;
