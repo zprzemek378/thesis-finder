f5936527a047a6fdf77c1a305fc6505b5eed21d6
Przemysław Zieliński <zprzemek378@gmail.com>
2025-05-07 18:01:50 +0200

    fix main layout

--- client/src/common/layout/MainLayout.tsx
+++ client/src/common/layout/MainLayout.tsx
@@ -8,9 +8,9 @@ interface MainLayoutProps {
 
 const MainLayout = ({ children }: MainLayoutProps) => {
   return (
-    <div className="flex flex-col w-full bg-[var(--o-gray-light)]">
+    <div className="flex flex-col min-h-screen w-full bg-[var(--o-gray-light)]">
       <Header />
-      <main className="flex-1 w-full p-8 flex flex-col">{children}</main>
+      <main className="flex-1 w-full p-8">{children}</main>
       <Footer />
     </div>
   );
