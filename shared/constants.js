6e93d68ee1c4747d31ecf5ed93dd0cbc3f8b5d91
Przemysław Zieliński <zprzemek378@gmail.com>
2025-06-09 00:14:07 +0200

    integrate register and login with backend

--- shared/constants.js
+++ shared/constants.js
@@ -1,1 +1,34 @@
 export const PORT = 3000;
+
+export const FACULTIES = [
+  { value: "WEAIiIB", label: "Wydział Elektrotechniki, Automatyki i Inżynierii Biomedycznej" },
+  { value: "WGGiIŚ", label: "Wydział Geodezji Górniczej i Inżynierii Środowiska" },
+  { value: "WGiG", label: "Wydział Górnictwa i Geoinżynierii" },
+  { value: "WIMiC", label: "Wydział Inżynierii Materiałowej i Ceramiki" },
+  { value: "WIMiIP", label: "Wydział Inżynierii Metali i Informatyki Przemysłowej" },
+  { value: "WIMiR", label: "Wydział Inżynierii Mechanicznej i Robotyki" },
+  { value: "WMS", label: "Wydział Matematyki Stosowanej" },
+  { value: "WO", label: "Wydział Odlewnictwa" },
+  { value: "WFiIS", label: "Wydział Fizyki i Informatyki Stosowanej" },
+  { value: "WWNiG", label: "Wydział Wiertnictwa, Nafty i Gazu" },
+  { value: "WZ", label: "Wydział Zarządzania" },
+  { value: "WEiP", label: "Wydział Energetyki i Paliw" },
+  { value: "WIEiT", label: "Wydział Informatyki, Elektroniki i Telekomunikacji" },
+  { value: "WH", label: "Wydział Humanistyczny" },
+];
+
+export const STUDIES_TYPES = [
+  { value: "BACHELOR", label: "Licencjackie" },
+  { value: "ENGINEERING", label: "Inżynierskie" },
+  { value: "MASTER", label: "Magisterskie" },
+  { value: "DOCTORATE", label: "Doktoranckie" },
+  { value: "POST-GRADUATE", label: "Podyplomowe" },
+  { value: "OTHER", label: "Inne" },
+];
+
+export const DEGREES = [
+  { value: "I", label: "I stopnia" },
+  { value: "II", label: "II stopnia" },
+  { value: "III", label: "III stopnia" },
+  { value: "jednolite", label: "Jednolite magisterskie" },
+];
